cf222b2eb18d4b49d060613f7b914fb136085e8f
Thepphithak Seekaew <95549790+Windmix@users.noreply.github.com>
2025-04-05 21:57:38 +0200

    update
    yes

---
+++ Assets/Scripts/cutsceneManager.cs
@@ -0,0 +1,54 @@
+﻿
+using TMPro;
+using UdonSharp;
+using UnityEditor.Rendering.PostProcessing;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class cutsceneManager : UdonSharpBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    [TextArea(2, 5)]
+    
+    public string[] dialogueLines;
+    private int currentLineIndex = 0;
+    private string currentLine = "";
+    private int currentCharIndex = 0;
+    public float letterDelay = 0.05f;
+
+    private bool isTyping = false;
+    void Start()
+    {
+        dialogueText.text = "";
+
+    }
+   
+    //}
+
+    public override void Interact()
+    {
+        if (isTyping) return;
+
+        currentLine = dialogueLines[currentLineIndex];
+        currentCharIndex = 0;
+        dialogueText.text = "";
+        isTyping = true;
+        ShowNextCharacter();
+
+        currentLineIndex = (currentLineIndex + 1) % dialogueLines.Length;
+    }
+    public void ShowNextCharacter()
+    {
+        if (currentCharIndex < currentLine.Length)
+        {
+            dialogueText.text += currentLine[currentCharIndex];
+            currentCharIndex++;
+            SendCustomEventDelayedSeconds(nameof(ShowNextCharacter), letterDelay);
+        }
+        else
+        {
+            isTyping = false;
+        }
+    }
+}
---
+++ Assets/Scripts/grave.cs
@@ -0,0 +1,35 @@
+﻿
+using SimpleAI;
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using static UnityEditor.FilePathAttribute;
+
+public class grave : UdonSharpBehaviour
+{
+    public GameObject teleportObject;
+    public GameObject previous;
+    public Animator animator;
+
+    public override void Interact()
+    {
+        if(Networking.LocalPlayer.isLocal)
+        {
+            Networking.LocalPlayer.TeleportTo(teleportObject.transform.position, Networking.LocalPlayer.GetRotation());
+            //animator.Play("FadeOut");
+           
+        }
+    }
+    void Update()
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.IsName("FadeOut") && stateInfo.normalizedTime >= 1.0f)
+        {
+          
+
+         
+        }
+    }
+}
