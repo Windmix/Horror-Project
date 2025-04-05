4329cf5ce64f2cb5ba11945fd86951d8bfde11c1
Thepphithak Seekaew <95549790+Windmix@users.noreply.github.com>
2025-04-05 12:52:05 +0200

    ok
    ok

---
+++ Assets/Scripts/DialougeBillboarded.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DialougeBillboarded : UdonSharpBehaviour
+{
+    private Vector3 playerCamera;
+    void Start()
+    {
+        if (Networking.LocalPlayer != null)
+        {
+            playerCamera = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+        }
+       
+    }
+    void Update()
+    {
+        playerCamera = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+        if (playerCamera != null)
+        {
+            transform.LookAt(playerCamera);
+            transform.Rotate(0, 180, 0); // Flip the UI to face the player properly
+        }
+    }
+}
